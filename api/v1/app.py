aa82ed6a8261ecd0136af5031d12d139a80e9a71
kevanlucc <kevan.lucchini@gmail.com>
2018-08-21 19:33:32 +0000

    add name to blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,10 @@ script starts Flask web app
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
-from Blueprint import register_blueprint
 
 
 app = Flask(__name__)
-app.register_blueprint("app_views", url_prefix="/api/v1")
+app.register_blueprint(app_views)
 app.url_map.strict_slashes=False
 
 
