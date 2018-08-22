fa7ecd85d011b5b291b95cd49bf3638fc1a95608
kevanlucc <kevan.lucchini@gmail.com>
2018-08-22 18:38:01 +0000

    added missing parameters to city instance creation

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,8 +6,8 @@ from api.v1.views import app_views
 from flask import make_response, jsonify, request, abort
 from models import storage
 from api.v1.app import not_found
-from models import City
 from models import State
+from models import City
 
 @app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
 def get_cities(state_id):
@@ -26,7 +26,7 @@ def get_cities(state_id):
         if 'name' not in request.json:
             abort(400, 'Missing name')
         data = request.get_json().get('name')
-        new_city = City(name=data)
+        new_city = City(name=data, state_id=state_id)
         new_city.save()
         return make_response(jsonify(new_city.to_dict()), 201)
 
@@ -58,7 +58,7 @@ def update_city(city_id):
             abort(400, 'Not a JSON')
         for k, v in request.json.items():
             if k not in ['updated_at', 'created_at', 'id']:
-                setattr(state, k, v)
+                setattr(city, k, v)
         city.save()
         data = city.to_dict()
         return make_response(jsonify(data), 200)
