02aa74622bbbc148223d44b5ed22cabef2122a08
kevanlucc <kevan.lucchini@gmail.com>
2018-08-22 17:08:03 +0000

    add pretty print to jsonify

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from flask import make_response
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
