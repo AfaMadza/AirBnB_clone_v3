ac11fe355489930cee3102e207cfcfe7410b54f1
Afa Madza <305@holbertonschool.com>
2018-08-24 01:40:05 +0000

    Fixed style errors and renamed functions

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -125,24 +125,24 @@ class testFileStorage(unittest.TestCase):
 
     def test_model_storage(self):
         '''
-            Test State model in Filestorage
+        Test for storage being instance of FileStorage
         '''
         self.assertTrue(isinstance(storage, FileStorage))
 
-    def test_get_file_storage(self):
-            """This test the get method in file_storage"""
+    def test_file_storage_get(self):
+            """Tests the get method in file_storage"""
             fs = FileStorage()
-            new_state = State()
-            fs.new(new_state)
-            first_state_id = list(storage.all("State").values())[0].id
-            self.assertEqual(type(storage.get("State", first_state_id)), State)
+            my_state = State()
+            fs.new(my_state)
+            first_id = list(storage.all("State").values())[0].id
+            self.assertEqual(type(storage.get("State", first_id)), State)
 
-    def test_count_file_storage(self):
-        """This test the get method in file_storage"""
+    def test_file_storage_count(self):
+        """Tests the count method in file_storage"""
         storage.reload()
-        result = storage.all("")
+        res = storage.all("")
         count = storage.count(None)
-        self.assertEqual(len(result), count)
-        result = storage.all("State")
+        self.assertEqual(len(res), count)
+        res = storage.all("State")
         count = storage.count("State")
-        self.assertEqual(len(result), count)
+        self.assertEqual(len(res), count)
